72e593d5d906be0970e6ff18a45b11d54d7c51f9
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-07-09 22:08:46 -0400

    move fake dbdata from Notes into own file so other compnents can use it

---
+++ src/data.js
@@ -0,0 +1,11 @@
+let dbData = {
+   notes: [
+     {time: 3, type: "code", content: "i am a \ncode"}, 
+     {time: 5, type: "text", content: "i am text"}, 
+     {time: 7, type: "pic", content: "https://media.giphy.com/media/1wqYNiObsge7Jowizv/giphy.gif", alt: "gif"}, 
+     {time: 9, type: "code", content: "i am \nmore \n\tcode"}, 
+     {time: 12, type: "pic", content: "https://static.boredpanda.com/blog/wp-content/uploads/2018/04/5acb63d83493f__700-png.jpg", alt: "cat pic"}
+   ]
+ }
+
+export default dbData
